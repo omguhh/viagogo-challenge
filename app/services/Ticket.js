08fc9bdf7c7a5fd558a7c77f65835393dbd959c1
omguhh <yasmine.m.f@live.co.uk>
2017-11-11 20:11:37 +0400

    added logic for finding cheapest ticket of a number of tickets

--- app/services/Ticket.js
+++ app/services/Ticket.js
@@ -8,17 +8,33 @@
 
         generateTicket: function () {
 
-            var ticketCost, ticketsAvailable, ticket;
+            var ticketCost, ticketsAvailable, ticket, tickets, cheapestTicket;
+
+            tickets = [];
 
             ticketsAvailable = Math.floor(Math.random() * (101));
             ticketCost = Math.floor(Math.random() * (101));
 
-            ticket  = {
-                costPerTicket: ticketCost,
-                ticketsAvailable: ticketsAvailable
-            };
+            for(var i = 0; i < ticketsAvailable; i++) {
 
-           return ticket;
+                ticketCost = Math.floor(Math.random() * (101));
+
+                ticket  = {
+                    ticketPrice: ticketCost
+                };
+
+                if (!cheapestTicket || ticket.ticketPrice < cheapestTicket) {
+                    cheapestTicket = ticket;
+                }
+
+                tickets.push(ticket);
+
+            }
+
+           return {
+               tickets: tickets,
+               cheapestTicket: cheapestTicket || 0
+           };
 
         }
         
